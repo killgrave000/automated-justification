e7b380eb05d6a8985ce60e0de0dfcc49ad8006ff
Mahir Labib <161746452+killgrave000@users.noreply.github.com>
2025-11-25 20:12:18 +0600

    Update bcbs_idr_app.py

--- bcbs_idr_app.py
+++ bcbs_idr_app.py
@@ -16,7 +16,7 @@ st.session_state.setdefault("download_ready", False)
 # -----------------------------
 # CONFIGURATION
 # -----------------------------
-GEMINI_API_KEY = "AIzaSyAyIOyIumz9NF8tW9vcdGoidfn0wc-NWGg"  # <-- Replace with your valid key
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]  # <-- Replace with your valid key
 import platform
 import shutil
 
